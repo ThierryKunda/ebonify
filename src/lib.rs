02b8a00776aac224807e1711c4e71950271dc707
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2022-08-26 23:47:38 +0200

    Clean lib + main

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,5 @@
+mod error;
+mod pre_teatment;
+mod ebnf_syntax;
 #[cfg(test)]
-mod tests {
-    use std::fs;
-
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-
-    // #[test]
-    // fn seperateLines() {
-        
-    // }
-}
+mod tests;
