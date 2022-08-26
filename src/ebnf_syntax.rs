fc2c5bbbd41e4b74cad03058561845269d6a51eb
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2022-08-26 23:46:59 +0200

    enum Token + correctif

--- src/ebnf_syntax.rs
+++ src/ebnf_syntax.rs
@@ -1,33 +1,28 @@
 pub enum Operator {
-    Alternation(String),
-    Optional(String, String),
-    RepetitionL(String),
-    RepetitionR(String),
-    GroupingL(String),
-    GroupingR(String),
-    TerminalStringL(String),
-    TerminalStrinR(String),
-    CommentL(String),
-    CommentR(String),
-    Exception(String),
-}
-
-pub struct Identifier {
-    symbol: String,
+    Alternation,
+    Concatenation,
+    Exception,
+    OptionalL,
+    OptionalR,
+    RepetitionL,
+    RepetitionR,
+    GroupingL,
+    GroupingR,
 }
 
 pub enum Rule {
     N,
-    Single(Identifier),
-    Capsule(Identifier, Operator, Identifier),
-    CapsCompo(Operator, Box<Rule>, Operator),
+    Literal(String),
+    Identifier(String),
+    Operation(Box<Rule>, Operator),
 }
 
-pub struct Association {
-    identifier: Identifier,
-    rule: Rule,
+pub enum Token {
+    Op(Operator),
+    Rl(Rule),
 }
 
 pub struct EBNF {
+    identifier: Rule,
     rules: Vec<Rule>
 }
