ebbc3c629f6b56951abedc46a3131ef91c6e200e
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2022-10-07 18:05:34 +0200

    Adding references possibility in Rule structure

--- src/ebnf_syntax.rs
+++ src/ebnf_syntax.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 pub enum Operator {
     Alternation,
     Concatenation,
@@ -10,21 +11,30 @@ pub enum Operator {
     GroupingR,
 }
 
-pub enum Rule {
+pub enum Rule<'a> {
     Literal(String),
     Identifier(String),
-    Dual(Box<Rule>, Operator, Box<Rule>),
-    Single(Operator, Box<Rule>, Operator),
-
+    Alternation(Box<Rule<'a>>, Box<Rule<'a>>),
+    AlterRef(Box<&'a Rule<'a>>, Box<&'a Rule<'a>>),
+    AlterRefL(Box<&'a Rule<'a>>, Box<Rule<'a>>),
+    AlterRefR(Box<Rule<'a>>, Box<&'a Rule<'a>>),
+    Concatenation(Box<Rule<'a>>, Box<Rule<'a>>),
+    ConcatRef(Box<&'a Rule<'a>>, Box<&'a Rule<'a>>),
+    ConcatRefL(Box<&'a Rule<'a>>, Box<Rule<'a>>),
+    ConcatRefR(Box<Rule<'a>>, Box<&'a Rule<'a>>),
+    Exception(Box<Rule<'a>>, Box<Rule<'a>>),
+    ExceptRef(Box<&'a Rule<'a>>, Box<&'a Rule<'a>>),
+    ExceptRefL(Box<&'a Rule<'a>>, Box<Rule<'a>>),
+    ExceptRefR(Box<Rule<'a>>, Box<&'a Rule<'a>>),
+    Optional(Box<Rule<'a>>),
+    OptRef(Box<&'a Rule<'a>>),
+    Repetition(Box<&'a Rule<'a>>),
+    RepetRef(Box<&'a Rule<'a>>),
+    GrpRef(Box<&'a Rule<'a>>),
 }
 
-pub enum Token {
+pub enum Token<'a> {
     Op(Operator),
-    Rl(Rule),
+    Rl(Rule<'a>),
     Invalid,
-}
-
-pub struct EBNF {
-    identifier: Rule,
-    rules: Vec<Rule>
 }
