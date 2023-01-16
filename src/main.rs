69f43c45d7c4d48a886ae804770edd5f6fa4356c
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-01-16 19:42:49 +0100

    Display of the general tree given by the example file

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,18 @@
+mod ebnf_syntax;
+mod error;
+mod pre_treatment;
+mod ast;
+mod structure;
+
+use structure::EbnfTree;
+
 fn main() {
+    let example = EbnfTree::from_file("tests_samples/lang_mini.ebnf");
+    if let Ok(tree) = example {
+        for (name, definition) in tree.rules {
+            println!("{} = {:?}\n", name, definition);
+        }
+    } else {
+        println!("Error for the example");
+    }
 }
