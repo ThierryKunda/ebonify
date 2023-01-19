57ef39bd52687bc2a4165e208a2520c2b91e8e29
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-01-19 22:07:11 +0100

    add identified_counts attribute

--- src/structure.rs
+++ src/structure.rs
@@ -1,6 +1,6 @@
 use std::ops::Deref;
 
-use crate::ast::{create_definition_tree, get_pure_tree};
+use crate::ast::{create_definition_tree, get_pure_tree, tree_with_id_ref};
 use crate::error::PreTreatmentError;
 use crate::pre_treatment::{split_members, split_lines, tokenize_rule_from_str, split_lines_from_file};
 use crate::ebnf_syntax::*;
@@ -11,6 +11,7 @@ pub struct EbnfTree {
     pub syntax_source_name: Option<String>,
     pub nodes_count: u32,
     pub rules: BTreeMap<String, Rc<Rule>>,
+    pub identified_counts: BTreeMap<String, u16>
 }
 
 impl EbnfTree {
@@ -19,6 +20,7 @@ impl EbnfTree {
             syntax_source_name: Some(source_name.to_string()),
             nodes_count: 0,
             rules: BTreeMap::new(),
+            identified_counts: BTreeMap::new()
         }
     }
 
@@ -63,26 +65,28 @@ impl From<&Vec<(String, Rc<Rule>)>> for EbnfTree {
             syntax_source_name: None,
             nodes_count: 0,
             rules: pairs,
+            identified_counts: BTreeMap::new(),
         }
     }
 }
 
 impl From<&str> for EbnfTree {
     fn from(text: &str) -> Self {
-    let mut pairs: BTreeMap<String, Rc<Rule>> = BTreeMap::new();
-    let mut rules_by_line = split_lines(text.to_string());
-    for r  in rules_by_line.iter_mut() {
-        r.pop();
-    }
-    let rulename_definition_pairs = split_members(rules_by_line);
-    for (rule_name, definition) in rulename_definition_pairs {
-        let tokens = tokenize_rule_from_str(definition);
-        pairs.insert(rule_name, create_definition_tree(&tokens));
-    }
+        let mut pairs: BTreeMap<String, Rc<Rule>> = BTreeMap::new();
+        let mut rules_by_line = split_lines(text.to_string());
+        for r  in rules_by_line.iter_mut() {
+            r.pop();
+        }
+        let rulename_definition_pairs = split_members(rules_by_line);
+        for (rule_name, definition) in rulename_definition_pairs {
+            let tokens = tokenize_rule_from_str(definition);
+            pairs.insert(rule_name, create_definition_tree(&tokens));
+        }
         EbnfTree {
             syntax_source_name: None,
             nodes_count: 0,
             rules: pairs,
+            identified_counts: BTreeMap::new()
         }
     }
 }
