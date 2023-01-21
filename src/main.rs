87fb26ae400cb1854d18a5be54a691afe6f851d2
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-01-21 22:50:26 +0100

    update_identified_counts called in main

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,14 @@ use structure::EbnfTree;
 
 fn main() {
     let example = EbnfTree::from_file("tests_samples/lang_mini.ebnf");
-    if let Ok(tree) = example {
+    if let Ok(mut tree) = example {
+        tree.update_identified_counts();
         for (name, definition) in tree.rules {
             println!("{} = {:?}\n", name, definition);
         }
+        println!("{:?}", tree.identified_counts);
     } else {
         println!("Error for the example");
     }
+
 }
