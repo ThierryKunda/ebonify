dc888b73dd0754be2270f752dbdbd9f97f9c200c
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-01-22 15:12:09 +0100

    update_all_definition_nodes_count called in main + display

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,12 @@ use structure::EbnfTree;
 fn main() {
     let example = EbnfTree::from_file("tests_samples/lang_mini.ebnf");
     if let Ok(mut tree) = example {
+        tree.update_all_definition_nodes_count(true);
         tree.update_identified_counts();
         for (name, definition) in tree.rules {
             println!("{} = {:?}\n", name, definition);
         }
-        println!("{:?}", tree.identified_counts);
+        println!("{:?}", tree.nodes_count_per_definition);
     } else {
         println!("Error for the example");
     }
