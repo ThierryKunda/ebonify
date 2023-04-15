2d458da8d9e30e3d9825b8ed3aef8edda66708d8
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-04-15 10:54:09 +0200

    Added from_data_format assoc. function

--- src/structure/builders.rs
+++ src/structure/builders.rs
@@ -1,4 +1,4 @@
-use crate::error::ParsingError;
+use crate::{error::{ParsingError, JSONParsingError}, data_formatting::Data};
 use super::tree::EbnfTree;
 
 pub struct EbnfTreeBuilder {
@@ -14,6 +14,13 @@ impl EbnfTreeBuilder {
     
     pub fn from_plain_text(text: &str) -> Result<EbnfTree, ParsingError> {
         let mut res = EbnfTree::from(text);
+        res.update_all_definition_nodes_count(true);
+        res.update_identified_counts();
+        Ok(res)
+    }
+
+    pub fn from_data_format(data: &impl Data) -> Result<EbnfTree, JSONParsingError> {
+        let mut res = EbnfTree::from_json(data.get_json())?;
         res.update_all_definition_nodes_count(true);
         res.update_identified_counts();
         Ok(res)
