24459ccb3982eeffe294da05b11d19ad6140d67b
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-04-17 21:46:53 +0200

    Added basic implementions of clap

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,13 @@
 // use std::path::PathBuf;
 
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, Args, ValueEnum};
+
+#[derive(Debug, Copy, Clone, ValueEnum)]
+pub enum SourceType {
+    JSON,
+    File,
+    HTTP,
+}
 
  #[derive(Parser, Debug)]
  #[command(author = "Thierry K. <kundathierry@gmail.com>", version = "1.0", about, long_about = None)]
@@ -8,13 +15,25 @@ pub struct Cli {
     pub name: Option<String>,
 
     #[command(subcommand)]
-    command: Option<Commands>,
+    pub command: Option<Commands>,
+}
+
+#[derive(Debug, Args)]
+pub struct ConvertArgs {
+    source_type: Option<SourceType>,
+    value: String
+}
+
+#[derive(Debug, Args)]
+pub struct MatchArgs {
+    syntax_source_type: Option<SourceType>,
+    value: String
 }
 
 #[derive(Debug, Subcommand)]
-enum Commands {
+pub enum Commands {
     // Converts some content and return back the result
-    Convert,
+    Convert(ConvertArgs),
     // Check if the syntax corresponds to the input content
-    Match,
+    Match(MatchArgs)
 }
