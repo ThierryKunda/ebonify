bf0715929d8e7101472a0d106dc6badbad01b1ff
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-04-18 18:56:59 +0200

    Added ConversionError struct

--- src/error.rs
+++ src/error.rs
@@ -13,7 +13,7 @@ impl Display for ParsingError {
 impl Error for ParsingError {}
 
 #[derive(Debug)]
-pub struct RuleError {}
+pub struct RuleError;
 
 impl Display for RuleError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -29,5 +29,25 @@ pub struct JSONParsingError;
 impl Display for JSONParsingError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "The JSON content is invalid")
+    }
+}
+#[derive(Debug)]
+pub struct ConversionError {
+    message: String
+}
+
+impl ConversionError {
+    pub fn new(message: &str) -> Self {
+        ConversionError { message: message.to_string() }
+    }
+}
+
+impl Error for ConversionError {
+
+}
+
+impl Display for ConversionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "The conversion between the two types is invalid.\n Reason : {}", self.message)
     }
 }
