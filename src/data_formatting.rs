5a6b8af2c4c1ffea0d44adedab36ae9150d18ee5
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-04-18 18:55:30 +0200

    JSON and HTTP representation of data

--- src/data_formatting.rs
+++ src/data_formatting.rs
@@ -1,5 +1,36 @@
 use serde_json::Value;
+use crate::error::ConversionError;
 
 pub trait Data {
-    fn get_json(&self) -> Value;
+    fn get_json(&self) -> Result<Value, ConversionError>;
+}
+
+struct JsonData {
+    value: Value
+}
+
+
+struct HttpData {
+    uri: String
+}
+
+
+impl Data for JsonData {
+    fn get_json(&self) -> Result<Value, ConversionError> {
+        Ok(self.value.clone())
+    }
+}
+
+impl Data for HttpData {
+    fn get_json(&self) -> Result<Value, ConversionError> {
+        let body = reqwest::blocking::get(&self.uri);
+        match body {
+            Ok(resp) => match resp.json::<Value>() {
+                Ok(v) => Ok(v),
+                Err(_) => Err(ConversionError::new("Error during data conversion to JSON")),
+            }
+            Err(e) => Err(ConversionError::new("Error from HTTP request")),
+        }
+
+    }
 }
