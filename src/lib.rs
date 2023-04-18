80f5061d5a890f3146b5a6342527dbcd2cf90599
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-04-18 22:47:52 +0200

    Changed visibility for error and cli modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 #![crate_name = "ebonify"]
-mod error;
+pub mod error;
 pub mod parsing;
 pub mod ebnf_syntax;
 pub mod ast;
 pub mod data_formatting;
 pub mod structure;
-#[allow(unused)]
+
 mod utils;
-mod cli;
+pub mod cli;
