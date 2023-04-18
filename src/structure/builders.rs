a1372bba553dda109434953563bdd1efe0d5e65e
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-04-18 19:00:47 +0200

    Replaced JSONPars..Error with Conv..Error

--- src/structure/builders.rs
+++ src/structure/builders.rs
@@ -1,4 +1,4 @@
-use crate::{error::{ParsingError, JSONParsingError}, data_formatting::Data};
+use crate::{error::{ParsingError, ConversionError}, data_formatting::Data};
 use super::tree::EbnfTree;
 
 pub struct EbnfTreeBuilder {
@@ -19,8 +19,8 @@ impl EbnfTreeBuilder {
         Ok(res)
     }
 
-    pub fn from_data_format(data: &impl Data) -> Result<EbnfTree, JSONParsingError> {
-        let mut res = EbnfTree::from_json(data.get_json())?;
+    pub fn from_data_format(data: &impl Data) -> Result<EbnfTree, ConversionError> {
+        let mut res = EbnfTree::from_json(data.get_json()?)?;
         res.update_all_definition_nodes_count(true);
         res.update_identified_counts();
         Ok(res)
